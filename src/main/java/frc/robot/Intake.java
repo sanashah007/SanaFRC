fe179c077aacd52a564920f5642798ae274f35a8
sanashah007 <54493565+sanashah007@users.noreply.github.com>
2021-10-29 16:23:15 -0700

    Initial commit (C2)

---
+++ src/main/java/frc/robot/Intake.java
@@ -0,0 +1,28 @@
+// package frc.robot;
+
+// public class Intake extends {
+    
+//     // private final XboxController xboxController = new XboxController(0);
+
+//     // // setting up buttons for tele
+//     // XboxController shootController = new XboxController(1);
+
+//     // public double getDriverAxis(int axis) {
+//     //     return shootController.getRawAxis(axis);
+
+//     // }
+
+//     // public void teleopPeriodic() {
+//     //     shooterMotorB.follow(shooterMotorA);
+
+//     //     //double leftY = xboxController.getY(GenericHID.Hand.kLeft);
+//     //     double rightY = xboxController.getX(GenericHID.Hand.kRight);
+
+//     //     shooterMotorA.set(ControlMode.PercentOutput, leftY);// setting speed of motors
+//     //     shooterMotorB.set(ControlMode.PercentOutput, rightY);
+    
+
+//     //}
+
+    
+// }
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,163 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import com.ctre.phoenix.ErrorCode;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private final XboxController xboxController = new XboxController(0); 
+  private TalonFX driveMotorLeftA = new TalonFX(1);
+  private TalonFX driveMotorLeftB = new TalonFX(2);
+  private TalonFX driveMotorRightA = new TalonFX(3);
+  private TalonFX driveMotorRightB = new TalonFX(4);
+  public TalonFX shooterMotorA = new TalonFX(9);
+  public TalonFX shooterMotorB = new TalonFX(10);
+  private TalonFX indexer = new TalonFX(8); 
+  //private CANSparkMax funnelRightMotor = new CANSparkMax(7, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private CANSparkMax rollerMotorA = new CANSparkMax(5, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    driveMotorRightA.follow(driveMotorRightB);
+    driveMotorLeftA.follow(driveMotorLeftB);
+    shooterMotorB.setInverted(true);
+    shooterMotorB.follow(shooterMotorA);
+    indexer.setInverted(true);
+    //double leftY = xboxController.getY(GenericHID.Hand.kLeft); 
+
+     
+  }
+      //Setting up buttons for tele
+  XboxController driverController = new XboxController(0);
+  public double getDriverAxis(int axis){
+    return driverController.getRawAxis(axis); 
+  }
+
+  
+
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+  }
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    double stick = xboxController.getRawAxis(1);
+    double leftY = xboxController.getY(GenericHID.Hand.kLeft); 
+    double rightY = xboxController.getY(GenericHID.Hand.kRight); 
+    if(leftY >= -0.1 && leftY <= 0.1){ //dead zone 
+      leftY =0; 
+    }
+    if(rightY >= -0.1 && rightY <= 0.1){ //dead zone 
+      rightY =0; 
+    }
+    driveMotorRightA.set(ControlMode.PercentOutput, leftY * -1);//setting speed of motors
+    driveMotorLeftA.set(ControlMode.PercentOutput, rightY);   
+    
+    if (xboxController.getBButton()) {
+      shooterMotorA.set(ControlMode.PercentOutput, 0.5);
+      indexer.set(ControlMode.PercentOutput, 0.5);
+    } else {
+      shooterMotorA.set(ControlMode.PercentOutput, 0);
+      indexer.set(ControlMode.PercentOutput, 0);
+    } 
+    if (xboxController.getAButton()) {
+      //funnelRightMotor.set(0.5);
+      rollerMotorA.set(0.5);
+    }   else {
+      //funnelRightMotor.set(0.0);
+      rollerMotorA.set(0.0);
+    } 
+
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
